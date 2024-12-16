ef9861c6a25d986d8be06e292b8218a8a0226494
RVCAUMO <rvcaumo@ext.worten.pt>
2024-12-16 15:19:04 +0000

    feat: containerize frontend

--- frontend/app/src/environments/environment.prod.ts
+++ frontend/app/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: '',
+  // @ts-ignore
+  apiURL: window["env"]["apiUrl"] ||  'http://localhost:8080',
   clientId: 'my-angular-app',
   clientSecret: '@321',
   obterTokenUrl: '/oauth/token'
